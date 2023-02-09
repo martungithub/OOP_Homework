870d43727c8d6dab1c55f7801427360bc2230bac
Martun Davitavyan <107114511+martungithub@users.noreply.github.com>
2023-02-10 01:34:51 +0400

    Add files via upload

---
+++ 09_02_2022_Excell_Spreadsheet/spreadsheet.py
@@ -0,0 +1,67 @@
+import cell
+
+
+class Spreadsheet:
+    def __init__(self, rows=0, column=0):
+        if isinstance(rows, int) and isinstance(column, int) and rows >= 0 and column >= 0:
+            self.cells = [[cell.Cell() for column in range(column)] for rows in range(rows)]
+        else:
+            raise Exception("Wrong input")
+
+    def set_cell_at(self, row, column, value):
+        if row >= 0 and column >= 0 and isinstance(row, int) and isinstance(column, int):
+            self.cells[row][column].value = value
+        else:
+            raise Exception("No rows or columns found in the matrix.")
+
+    def get_cell_at(self, row, column):
+        return self.cells[row][column]
+
+    def add_row(self, position):
+        if 0 <= position <= len(self.cells):
+            self.cells = self.cells[:position] + [[cell.Cell()] * len(self.cells[0])] + self.cells[position:]
+        else:
+            raise Exception("No rows found in the matrix.")
+
+    def add_column(self, position):
+        if 0 <= position <= len(self.cells[0]):
+            for i in range(len(self.cells)):
+                self.cells[i].insert(position, cell.Cell())
+        else:
+            raise Exception("No columns found in the matrix.")
+
+    def swap_rows(self, row1, row2):
+        self.cells[row1], self.cells[row2] = self.cells[row2], self.cells[row2]
+
+    def swap_columns(self, column1, column2):
+        for row in self.cells:
+            row[column1], row[column2] = row[column2], row[column1]
+
+    def remove_row(self, row):
+        if 0 <= row < len(self.cells):
+            del self.cells[row]
+        else:
+            raise Exception("No rows found in the matrix.")
+
+    def remove_column(self, column):
+        if 0 <= column < len(self.cells[0]):
+            self.cells = [row[:column] + row[column + 1:] for row in self.cells]
+        else:
+            raise Exception("No columns found in the matrix.")
+
+    def print(self):
+        for row in self.cells:
+            for value in row:
+                print(value, value.value, end=" ")
+            print()
+
+
+# spreadsheet1 = Spreadsheet()
+# spreadsheet1.set_cell_at(0, 0, 3)
+# spreadsheet1.set_cell_at(0, 1, "Matt")
+# spreadsheet1.set_cell_at(1, 0, "Wilson")
+# spreadsheet1.set_cell_at(1, 1, 24)
+# spreadsheet1.add_row(1)
+# spreadsheet1.add_column(1)
+# print(spreadsheet1.get_cell_at(1, 1).value)
+# spreadsheet1.print()
---
+++ 09_02_2022_Excell_Spreadsheet/test.py
@@ -0,0 +1,44 @@
+import cell
+import spreadsheet
+
+
+class Test:
+    def test_set_cell_at(self):
+        test_spreadsheet = spreadsheet.Spreadsheet(2, 3)
+        if len(test_spreadsheet.cells) == 2 and len(test_spreadsheet.cells[0]) == 3:
+            print("Test set cell at successfully passed")
+        else:
+            print("Test set cell at failed")
+
+    def test_get_cell_at(self):
+        test_spreadsheet = spreadsheet.Spreadsheet(2, 3)
+        if isinstance(test_spreadsheet.get_cell_at(1, 2), cell.Cell):
+            print("Test set cell at successfully passed")
+        else:
+            print("Test set cell at failed")
+
+    def test_add_row(self):
+        test_spreadsheet = spreadsheet.Spreadsheet(2, 3)
+        initial_rows = len(test_spreadsheet.cells)
+        test_spreadsheet.add_row(1)
+        if initial_rows == len(test_spreadsheet.cells) - 1:
+            print("Test set cell at successfully passed")
+        else:
+            print("Test set cell at failed")
+
+    def test_add_column(self):
+        test_spreadsheet = spreadsheet.Spreadsheet(2, 3)
+        initial_columns = len(test_spreadsheet.cells[0])
+        test_spreadsheet.add_column(1)
+        if initial_columns == len(test_spreadsheet.cells[0]) - 1:
+            print("Test set cell at successfully passed")
+        else:
+            print("Test set cell at failed")
+
+
+
+test = Test()
+test.test_set_cell_at()
+test.test_get_cell_at()
+test.test_add_row()
+test.test_add_column()
